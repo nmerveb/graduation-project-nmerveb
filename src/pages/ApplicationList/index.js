1d4d31615e5f767a7d3b7aba819af260c93c6c09
Merve Bacak <nmerveb@gmail.com>
2021-08-24 17:39:53 +0300

    Feat: Application layout created

---
+++ src/pages/ApplicationList/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./ApplicationList.module.css";
+
+function ApplicatonList() {
+  return (
+    <>
+      <div className={styles.ApplicationListContainer}>
+        <div className={styles.ApplicationListTitleContainer}>
+          <span className={styles.ApplicationListTitle}>Başvurular</span>
+          <button className={styles.BackBtn}>Geri Dön ></button>
+        </div>
+        <div className={styles.ApplicatonList}>
+          <div className={styles.Application}>
+            <span className={styles.Applicant}>Merve Bacak</span>
+            <span className={styles.ApplicatonDate}>12.02.1234</span>
+            <button className={styles.ViewBtn}>Görüntüle</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ApplicatonList;
