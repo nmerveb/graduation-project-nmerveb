28d4e7de19dec258f76da674e2e7c506bfe44c6e
Merve Bacak <nmerveb@gmail.com>
2021-08-25 14:29:38 +0300

    Feat: Applcation ui design added.

--- src/pages/ApplicationList/index.js
+++ src/pages/ApplicationList/index.js
@@ -7,10 +7,58 @@ function ApplicatonList() {
       <div className={styles.ApplicationListContainer}>
         <div className={styles.ApplicationListTitleContainer}>
           <span className={styles.ApplicationListTitle}>Başvurular</span>
-          <button className={styles.BackBtn}>Geri Dön ></button>
+          <button className={styles.BackBtn}>Geri Dön</button>
         </div>
         <div className={styles.ApplicatonList}>
           <div className={styles.Application}>
+            <span className={styles.Applicant}>Merve Bacak</span>
+            <span className={styles.ApplicatonDate}>12.02.1234</span>
+            <button className={styles.ViewBtn}>Görüntüle</button>
+          </div>
+          <div className={styles.Application}>
+            <span className={styles.Applicant}>Merve Bacak</span>
+            <span className={styles.ApplicatonDate}>12.02.1234</span>
+            <button className={styles.ViewBtn}>Görüntüle</button>
+          </div>
+          <div className={styles.Application}>
+            <span className={styles.Applicant}>Merve Bacak</span>
+            <span className={styles.ApplicatonDate}>12.02.1234</span>
+            <button className={styles.ViewBtn}>Görüntüle</button>
+          </div>
+          <div className={styles.Application}>
+            <span className={styles.Applicant}>Merve Bacak</span>
+            <span className={styles.ApplicatonDate}>12.02.1234</span>
+            <button className={styles.ViewBtn}>Görüntüle</button>
+          </div>
+          <div className={styles.Application}>
+            <span className={styles.Applicant}>Merve Bacak</span>
+            <span className={styles.ApplicatonDate}>12.02.1234</span>
+            <button className={styles.ViewBtn}>Görüntüle</button>
+          </div>          <div className={styles.Application}>
+            <span className={styles.Applicant}>Merve Bacak</span>
+            <span className={styles.ApplicatonDate}>12.02.1234</span>
+            <button className={styles.ViewBtn}>Görüntüle</button>
+          </div>          <div className={styles.Application}>
+            <span className={styles.Applicant}>Merve Bacak</span>
+            <span className={styles.ApplicatonDate}>12.02.1234</span>
+            <button className={styles.ViewBtn}>Görüntüle</button>
+          </div>          <div className={styles.Application}>
+            <span className={styles.Applicant}>Merve Bacak</span>
+            <span className={styles.ApplicatonDate}>12.02.1234</span>
+            <button className={styles.ViewBtn}>Görüntüle</button>
+          </div>          <div className={styles.Application}>
+            <span className={styles.Applicant}>Merve Bacak</span>
+            <span className={styles.ApplicatonDate}>12.02.1234</span>
+            <button className={styles.ViewBtn}>Görüntüle</button>
+          </div>          <div className={styles.Application}>
+            <span className={styles.Applicant}>Merve Bacak</span>
+            <span className={styles.ApplicatonDate}>12.02.1234</span>
+            <button className={styles.ViewBtn}>Görüntüle</button>
+          </div>          <div className={styles.Application}>
+            <span className={styles.Applicant}>Merve Bacak</span>
+            <span className={styles.ApplicatonDate}>12.02.1234</span>
+            <button className={styles.ViewBtn}>Görüntüle</button>
+          </div>          <div className={styles.Application}>
             <span className={styles.Applicant}>Merve Bacak</span>
             <span className={styles.ApplicatonDate}>12.02.1234</span>
             <button className={styles.ViewBtn}>Görüntüle</button>
