8fe59321974ea50a6f18f50fd90ff743a332ddc5
Merve Bacak <nmerveb@gmail.com>
2021-08-26 00:46:27 +0300

    Feat: Query Code ui design added.

--- src/pages/QueryCode/index.js
+++ src/pages/QueryCode/index.js
@@ -7,26 +7,29 @@ function QueryCode() {
       <div className={styles.Navbar}>
         <div className={styles.NavbarContentContainer}>
           <div className={styles.NavbarContent}>
-            <span className={styles.NavbarIcon}>Icon</span>
-            <span className={styles.NavbarTitle}>Title</span>
+            <span className={styles.NavbarTitle}>Başvuru Formu</span>
           </div>
           <div className={styles.NavbarContent}>
-            <span className={styles.NavbarIcon}>Icon</span>
-            <span className={styles.NavbarTitle}>Title</span>
+            <span className={styles.NavbarTitle}>Başvuru Sorgula</span>
           </div>
         </div>
         <div className={styles.NavbarLoginButton}>
-          <span className={styles.NavbarIcon}>Icon</span>
-          <button className={styles.LoginButton}>Yetkili Girişi</button>
+          <button className={styles.LoginButton}>
+            <span className={styles.NavbarLoginIcon}></span>Yetkili Girişi
+          </button>
         </div>
       </div>
       <div className={styles.QueryContainer}>
         <div className={styles.QueryContainerTitle}>
           <div className={styles.Icon}></div>
-          <div className={styles.Title}>Başvuru sorgula</div>
+          <div className={styles.Title}>Başvuru Sorgula</div>
         </div>
         <div className={styles.QueryCodeInputContainer}>
-          <input className={styles.QueryCodeInput} type="text" placeholder="Kodunuzu girin"/>
+          <input
+            className={styles.QueryCodeInput}
+            type="text"
+            placeholder="Kodunuzu girin"
+          />
         </div>
         <div className={styles.QueryBtnContainer}>
           <button className={styles.QueryButton}>Query</button>
