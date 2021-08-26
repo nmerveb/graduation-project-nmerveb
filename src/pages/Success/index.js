c14136949148e18fd45f9e90f63a1355829b9096
Merve Bacak <nmerveb@gmail.com>
2021-08-26 07:52:23 +0300

    Feat: Success ui design added.

--- src/pages/Success/index.js
+++ src/pages/Success/index.js
@@ -7,23 +7,21 @@ function Success() {
       <div className={styles.Navbar}>
         <div className={styles.NavbarContentContainer}>
           <div className={styles.NavbarContent}>
-            <span className={styles.NavbarIcon}>Icon</span>
-            <span className={styles.NavbarTitle}>Title</span>
+            <span className={styles.NavbarTitle}>Başvuru Formu</span>
           </div>
           <div className={styles.NavbarContent}>
-            <span className={styles.NavbarIcon}>Icon</span>
-            <span className={styles.NavbarTitle}>Title</span>
+            <span className={styles.NavbarTitle}>Başvuru Sorgula</span>
           </div>
         </div>
         <div className={styles.NavbarLoginButton}>
-          <span className={styles.NavbarIcon}>Icon</span>
-          <button className={styles.LoginButton}>Yetkili Girişi</button>
+          <button className={styles.LoginButton}>
+            <span className={styles.NavbarLoginIcon}></span>
+            Yetkili Girişi
+          </button>
         </div>
       </div>
       <div className={styles.SuccessFormContainer}>
-        <div className={styles.SuccessFormTitle}>
-            İşlem Başarılı
-        </div>
+        <div className={styles.SuccessFormTitle}>İşlem Başarılı</div>
         <div className={styles.SuccessFormContent}>
           <div className={styles.FormInfo}>
             <div className={styles.FormInfoTitle}>Başvuru Bilgileri</div>
@@ -38,19 +36,21 @@ function Success() {
             <div className={styles.FormInfoContent}>
               <span className={styles.FormLabel}>TC</span>
               <span className={styles.FormInput}>12345678912</span>
-            </div> 
+            </div>
             <div className={styles.FormInfoContent}>
               <span className={styles.FormLabel}>Yaş</span>
               <span className={styles.FormInput}>12</span>
             </div>
             <div className={styles.FormDescriptionContent}>
-              <span className={styles.FormDescriptionLabel}>Başvuru nedeni</span>
+              <span className={styles.FormDescriptionLabel}>
+                Başvuru nedeni
+              </span>
               <span className={styles.FormDescriptionInput}>123456789</span>
-            </div> 
+            </div>
             <div className={styles.FormDescriptionContent}>
               <span className={styles.FormDescriptionLabel}>Address</span>
               <span className={styles.FormDescriptionInput}>123456789</span>
-            </div> 
+            </div>
             <div className={styles.FormInfoContent}>
               <span className={styles.FormLabel}>Secilen Görsel</span>
               <span className={styles.FormInput}>görseladi.jpeg</span>
@@ -58,8 +58,13 @@ function Success() {
           </div>
           <div className={styles.QueryCodeInfo}>
             <div className={styles.QueryCodeTitle}>Teşekkürler</div>
-            <div className={styles.QueryCodeDescription}>Başvurunuz alınmıştır.<br/><br/>
-            Aşağıda belitirlen kod ile başvurunuzun durumunu kontrol edebilirsiniz</div>
+            <div className={styles.QueryCodeDescription}>
+              Başvurunuz alınmıştır.
+              <br />
+              <br />
+              Aşağıda belitirlen kod ile başvurunuzun durumunu kontrol
+              edebilirsiniz
+            </div>
             <div className={styles.QueryCode}> ndsmfbsdhs</div>
           </div>
         </div>
