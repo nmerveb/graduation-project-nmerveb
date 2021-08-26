60fb8a0ef244524505937471adc8da2491833674
Merve Bacak <nmerveb@gmail.com>
2021-08-27 00:03:51 +0300

    Feat: Eslint added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import Form from './pages/Form'
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Form />
-    </div>
-
-  );
+  const a = 12;
+  return <div className="App">{a}</div>;
 }
 
 export default App;
