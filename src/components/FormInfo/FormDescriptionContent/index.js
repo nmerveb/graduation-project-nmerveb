879c6cbba80e1593bc35335e228b07972cd3876f
Merve Bacak <nmerveb@gmail.com>
2021-08-27 16:50:34 +0300

    Fix: Form Description Content seperated to components

--- src/components/FormInfo/FormDescriptionContent/index.js
+++ src/components/FormInfo/FormDescriptionContent/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import FormDescriptionLabel from './FormDescriptionLabel';
+import FormDescriptionInput from './FormDescriptionInput';
 import styles from '../FormInfo.module.css';
 
 function FormDescriptionContent({ label, input }) {
   return (
     <div className={styles.FormDescriptionContent}>
-      <span className={styles.FormDescriptionLabel}>Başvuru nedeni</span>
-      <span className={styles.FormDescriptionInput}>123456789</span>
+      <FormDescriptionLabel label={label} />
+      <FormDescriptionInput input={input} />
     </div>
   );
 }
