6d6070c6680bb4115cb4760bfd1ae3ade4371895
Merve Bacak <nmerveb@gmail.com>
2021-08-27 22:33:26 +0300

    Fix: Admin Application Details seperated to components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import ApplicationDetails from './pages/ApplicationDetails';
+import AdminApplicationDetails from './pages/AdminApplicationDetails';
 
 function App() {
   return (
     <div className="App">
-      <ApplicationDetails />
+      <AdminApplicationDetails />
     </div>
   );
 }
--- src/pages/AdminApplicationDetails/index.js
+++ src/pages/AdminApplicationDetails/index.js
@@ -1,60 +1,14 @@
-import React from "react";
-import styles from "./AdminApplicationDetails.module.css";
+import React from 'react';
+import FormInfo from '../../components/FormInfo';
+import StatusUpdate from './StatusUpdate';
+import styles from './AdminApplicationDetails.module.css';
 
 function AdminApplicationDetails() {
   return (
     <>
       <div className={styles.ApplicationDetailsContainer}>
-        <div className={styles.ApplicationInfo}>
-          <div className={styles.ApplicationInfoTitle}>Başvuru Bilgileri</div>
-          <div className={styles.ApplicationInfoContent}>
-            <span className={styles.InfoLabel}>Ad</span>
-            <span className={styles.InfoInput}>Merve</span>
-          </div>
-          <div className={styles.ApplicationInfoContent}>
-            <span className={styles.InfoLabel}>Soyad</span>
-            <span className={styles.InfoInput}>Bacak</span>
-          </div>
-          <div className={styles.ApplicationInfoContent}>
-            <span className={styles.InfoLabel}>TC</span>
-            <span className={styles.InfoInput}>12345678912</span>
-          </div>
-          <div className={styles.ApplicationInfoContent}>
-            <span className={styles.InfoLabel}>Yaş</span>
-            <span className={styles.InfoInput}>12</span>
-          </div>
-          <div className={styles.InfoDescriptionContent}>
-            <span className={styles.InfoDescriptionLabel}>Başvuru Nedeni</span>
-            <span className={styles.InfoDescriptionInput}>
-              123456789213456789
-            </span>
-          </div>
-          <div className={styles.InfoDescriptionContent}>
-            <span className={styles.InfoDescriptionLabel}>Address</span>
-            <span className={styles.InfoDescriptionInput}>
-              123456789213456789
-            </span>
-          </div>
-          <div className={styles.InfoDescriptionContent}>
-            <span className={styles.InfoDescriptionLabel}>
-            Güncel Durum
-            </span>
-            <span className={styles.InfoDescriptionInput}>
-              123456789213456789
-            </span>
-          </div>
-          <div className={styles.ApplicationInfoContent}>
-            <span className={styles.InfoLabel}>Seçilen Görsel</span>
-            <span className={styles.InfoInput}>Görsel.jpeg</span>
-          </div>
-        </div>
-        <div className={styles.StatusUpdate}>
-          <div className={styles.StatusUpdateTitle}>Güncel Durum</div>
-          <textarea className={styles.StatusUpdateInput} type="text" />
-          <div className={styles.UpdateBtnContainer}>
-            <button className={styles.UpdateBtn}>Update</button>
-          </div>
-        </div>
+        <FormInfo />
+        <StatusUpdate />
       </div>
     </>
   );
