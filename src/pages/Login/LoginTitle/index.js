1134442f26d5e39ca8ca5485c43697c313da3369
Merve Bacak <nmerveb@gmail.com>
2021-08-27 13:43:58 +0300

    Feat: Login Title component created

---
+++ src/pages/Login/LoginTitle/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../Login.module.css';
+
+function LoginTitle() {
+  return <div className={styles.LoginTitle}>Yetkili Girişi</div>;
+}
+
+export default LoginTitle;
