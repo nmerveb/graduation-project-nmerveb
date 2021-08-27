a09c2505bc16a64b8b2299e49558193c545e8f81
Merve Bacak <nmerveb@gmail.com>
2021-08-27 09:05:43 +0300

    Fix: Firebase config

---
+++ src/config/firebase.js
@@ -0,0 +1,7 @@
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FİREBASE_API_KEY,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  appId: process.env.REACT_APP_ID,
+};
+
+export default firebaseConfig;
