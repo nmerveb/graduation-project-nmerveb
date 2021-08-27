b89490424a8b567afa252faa385b338d71d2a49a
Merve Bacak <nmerveb@gmail.com>
2021-08-27 17:38:00 +0300

    Feat: Query Code component created.

---
+++ src/pages/Success/QueryCodeInfo/QueryCode/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../../Success.module.css';
+
+function QueryCode({ code }) {
+  return <div className={styles.QueryCode}> {code}</div>;
+}
+
+export default QueryCode;
