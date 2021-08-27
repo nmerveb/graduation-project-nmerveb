4c3df9bed7342816e8dad607d32afc6beeaef690
Merve Bacak <nmerveb@gmail.com>
2021-08-27 14:18:11 +0300

    Fix:Login page seperated to components.

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,25 +1,20 @@
-import React from "react";
-import styles from "./Login.module.css";
+import React from 'react';
+import LoginIcon from './LoginIcon';
+import LoginTitle from './LoginTitle';
+import InputContent from './InputContent';
+import ButtonContent from './ButtonContent';
+import styles from './Login.module.css';
 
 function Login() {
   return (
     <>
       <div className={styles.LoginContainer}>
         <div className={styles.LoginBox}>
-          <div className={styles.LoginIcon}></div>
-          <div className={styles.LoginTitle}>Yetkili Girişi</div>
-          <div className={styles.LoginContent}>
-            <div className={styles.InputLabel}>Kullanıcı Adı</div>
-            <input className={styles.Input} type="text" placeholder="Kullanıcı adınızı giriniz."></input>
-          </div>
-          <div className={styles.LoginContent}>
-            <div className={styles.InputLabel}>Şifre</div>
-            <input className={styles.Input} type="password" placeholder="Şifrenizi giriniz"></input>
-          </div>
-          <div className={styles.ButtonContent}>
-            <button className={styles.Button}>Back</button>
-            <button className={styles.Button}>Login</button>
-          </div>
+          <LoginIcon />
+          <LoginTitle />
+          <InputContent label="Kullanıcı Adı" placeholder="Kullanıcı adınızı giriniz." />
+          <InputContent label="Şifre" placeholder="Şifrenizi giriniz." />
+          <ButtonContent />
         </div>
       </div>
     </>
