7a72e8a420f7c10ecf818747617b3ad29b6370f2
Merve Bacak <nmerveb@gmail.com>
2021-08-27 17:33:07 +0300

    Feat: Query Code Title component created.

---
+++ src/pages/Success/QueryCodeInfo/QueryCodeTitle/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../../Success.module.css';
+
+function QueryCodeTitle() {
+  return <div className={styles.QueryCodeTitle}>Teşekkürler</div>;
+}
+
+export default QueryCodeTitle;
