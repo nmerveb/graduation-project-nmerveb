d411b5c51ab25b8c6854eb94fc4951f36950d473
Merve Bacak <nmerveb@gmail.com>
2021-08-27 17:35:44 +0300

    Feat: Query Code Description component created.

---
+++ src/pages/Success/QueryCodeInfo/QueryCodeDescription/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from '../../Success.module.css';
+
+function QueryCodeDescription() {
+  return (
+    <div className={styles.QueryCodeDescription}>
+      Başvurunuz alınmıştır.
+      <br />
+      <br />
+      Aşağıda belitirlen kod ile başvurunuzun durumunu kontrol edebilirsiniz
+    </div>
+  );
+}
+
+export default QueryCodeDescription;
