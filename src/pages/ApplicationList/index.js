eead20d902c6af9cb70b175e861409410e9bcaea
Merve Bacak <nmerveb@gmail.com>
2021-08-27 20:18:42 +0300

    Fix: Application List seperated to components.

--- src/pages/ApplicationList/index.js
+++ src/pages/ApplicationList/index.js
@@ -1,5 +1,5 @@
-import React from "react";
-import styles from "./ApplicationList.module.css";
+import React from 'react';
+import styles from './ApplicationList.module.css';
 
 function ApplicatonList() {
   return (
@@ -7,61 +7,17 @@ function ApplicatonList() {
       <div className={styles.ApplicationListContainer}>
         <div className={styles.ApplicationListTitleContainer}>
           <span className={styles.ApplicationListTitle}>Başvurular</span>
-          <button className={styles.BackBtn}>Geri Dön</button>
+          <button className={styles.BackBtn} type="button">
+            Geri Dön
+          </button>
         </div>
         <div className={styles.ApplicatonList}>
           <div className={styles.Application}>
             <span className={styles.Applicant}>Merve Bacak</span>
             <span className={styles.ApplicatonDate}>12.02.1234</span>
-            <button className={styles.ViewBtn}>Görüntüle</button>
-          </div>
-          <div className={styles.Application}>
-            <span className={styles.Applicant}>Merve Bacak</span>
-            <span className={styles.ApplicatonDate}>12.02.1234</span>
-            <button className={styles.ViewBtn}>Görüntüle</button>
-          </div>
-          <div className={styles.Application}>
-            <span className={styles.Applicant}>Merve Bacak</span>
-            <span className={styles.ApplicatonDate}>12.02.1234</span>
-            <button className={styles.ViewBtn}>Görüntüle</button>
-          </div>
-          <div className={styles.Application}>
-            <span className={styles.Applicant}>Merve Bacak</span>
-            <span className={styles.ApplicatonDate}>12.02.1234</span>
-            <button className={styles.ViewBtn}>Görüntüle</button>
-          </div>
-          <div className={styles.Application}>
-            <span className={styles.Applicant}>Merve Bacak</span>
-            <span className={styles.ApplicatonDate}>12.02.1234</span>
-            <button className={styles.ViewBtn}>Görüntüle</button>
-          </div>          <div className={styles.Application}>
-            <span className={styles.Applicant}>Merve Bacak</span>
-            <span className={styles.ApplicatonDate}>12.02.1234</span>
-            <button className={styles.ViewBtn}>Görüntüle</button>
-          </div>          <div className={styles.Application}>
-            <span className={styles.Applicant}>Merve Bacak</span>
-            <span className={styles.ApplicatonDate}>12.02.1234</span>
-            <button className={styles.ViewBtn}>Görüntüle</button>
-          </div>          <div className={styles.Application}>
-            <span className={styles.Applicant}>Merve Bacak</span>
-            <span className={styles.ApplicatonDate}>12.02.1234</span>
-            <button className={styles.ViewBtn}>Görüntüle</button>
-          </div>          <div className={styles.Application}>
-            <span className={styles.Applicant}>Merve Bacak</span>
-            <span className={styles.ApplicatonDate}>12.02.1234</span>
-            <button className={styles.ViewBtn}>Görüntüle</button>
-          </div>          <div className={styles.Application}>
-            <span className={styles.Applicant}>Merve Bacak</span>
-            <span className={styles.ApplicatonDate}>12.02.1234</span>
-            <button className={styles.ViewBtn}>Görüntüle</button>
-          </div>          <div className={styles.Application}>
-            <span className={styles.Applicant}>Merve Bacak</span>
-            <span className={styles.ApplicatonDate}>12.02.1234</span>
-            <button className={styles.ViewBtn}>Görüntüle</button>
-          </div>          <div className={styles.Application}>
-            <span className={styles.Applicant}>Merve Bacak</span>
-            <span className={styles.ApplicatonDate}>12.02.1234</span>
-            <button className={styles.ViewBtn}>Görüntüle</button>
+            <button className={styles.ViewBtn} type="button">
+              Görüntüle
+            </button>
           </div>
         </div>
       </div>
