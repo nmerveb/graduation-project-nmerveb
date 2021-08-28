178d905383f864b3fe607693b449b55bdb25d8b4
Merve Bacak <nmerveb@gmail.com>
2021-08-28 15:54:13 +0300

    Fix: Routing success page with props fixed.

--- src/pages/Form/index.js
+++ src/pages/Form/index.js
@@ -1,31 +1,45 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useHistory } from 'react-router-dom';
+import FormSchema from '../../schema/FormSchema';
+import { AddApplication } from '../../services/fireStore';
 import Navbar from '../../components/Navbar';
 import styles from './Form.module.css';
 
 function Form() {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => console.log(data);
+  let querycode = '';
+  const history = useHistory();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(FormSchema),
+  });
+  const onSubmit = (formData) => {
+    querycode = AddApplication(formData);
+    history.push({
+      pathname: '/basvuru-basarili',
+      state: { queryCode: querycode },
+    });
+  };
   return (
     <>
       <Navbar />
       <form onSubmit={handleSubmit(onSubmit)} className={styles.FormContainer}>
         <div className={styles.FormTitle}>Başvuru Formu</div>
         <div className={styles.FormInputLabel}>Ad</div>
-        <input
-          className={styles.FormInput}
-          type="text"
-          placeholder="Adınızı giriniz"
-          {...register('name')}
-        />
+        <input className={styles.FormInput} placeholder="Adınızı giriniz" {...register('name')} />
+        <p className={styles.FormValidationError}>{errors.name?.message}</p>
         <div className={styles.FormInputLabel}>Soyad</div>
         <input
           className={styles.FormInput}
-          type="text"
           placeholder="Soyadınızı giriniz"
           {...register('surname')}
         />
+        <p className={styles.FormValidationError}>{errors.surname?.message}</p>
         <div className={styles.FormInputLabel}>TC</div>
         <input
           className={styles.FormInput}
@@ -33,8 +47,10 @@ function Form() {
           placeholder="TC kimlik numaranızı giriniz"
           {...register('TC')}
         />
+        <p className={styles.FormValidationError}>{errors.TC?.message}</p>
         <div className={styles.FormInputLabel}>Yaş</div>
         <input className={styles.FormInput} type="number" placeholder="Yaş" {...register('age')} />
+        <p className={styles.FormValidationError}>{errors.age?.message}</p>
         <div className={styles.InputDescriptionContainer}>
           <div className={styles.DescriptionLabel}>Adres</div>
           <textarea
@@ -42,6 +58,7 @@ function Form() {
             placeholder="Adres"
             {...register('address')}
           />
+          <p className={styles.FormValidationError}>{errors.address?.message}</p>
         </div>
         <div className={styles.InputDescriptionContainer}>
           <div className={styles.DescriptionLabel}>Açıklama</div>
@@ -50,19 +67,7 @@ function Form() {
             placeholder="Başvuru nedeninizi yazınız."
             {...register('reason')}
           />
-        </div>
-        <div className={styles.FormFileLabel}>Kartınızı özelleştirmek için görsel seçiniz</div>
-        <div className={styles.FormFileContainer}>
-          <label className={styles.FormFileInput} htmlFor="input-file">
-            <input
-              type="file"
-              id="input-file"
-              accept="image/png, image/jpeg"
-              hidden
-              {...register('image')}
-            />
-            Görsel Seç
-          </label>
+          <p className={styles.FormValidationError}>{errors.reason?.message}</p>
         </div>
         <div className={styles.ButtonContainer}>
           <button type="submit" className={styles.SendBtn}>
