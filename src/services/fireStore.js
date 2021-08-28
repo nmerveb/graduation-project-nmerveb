0cfb0155df2e8b93a64ffc8a3efb72a0a5af69d0
Merve Bacak <nmerveb@gmail.com>
2021-08-28 09:02:24 +0300

    Feat: AddApplication function added.

--- src/services/fireStore.js
+++ src/services/fireStore.js
@@ -1,14 +1,29 @@
 import firebase from './firebase';
+import codeGenerator from '../utils/codeGenerator';
 
 const db = firebase.firestore();
 
-function add() {
-  db.collection('application-list').add({
-    name: 'deneme',
-    surname: 'test',
-    age: '22',
-    address: 'test2',
-  });
+export function AddApplication(application) {
+  const code = codeGenerator();
+  const firstStatus = 'Başvuru inceleniyor';
+  db.collection('application-list')
+    .add({
+      Id: code,
+      name: application.name,
+      surname: application.surname,
+      TC: application.TC,
+      age: application.age,
+      address: application.address,
+      reason: application.reason,
+      status: firstStatus,
+      description: '',
+    })
+    .catch((error) => {
+      console.error('Error adding document: ', error);
+    });
+  return code;
 }
+
+function add() {}
 
 export default add;
