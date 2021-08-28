6cb0c91ae3acdd6b098f898a24e104035a0487f7
Merve Bacak <nmerveb@gmail.com>
2021-08-28 07:59:51 +0300

    Feat: Routing added to buttons.

--- src/pages/Login/ButtonContent/index.js
+++ src/pages/Login/ButtonContent/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import styles from '../Login.module.css';
 
 function ButtonContent() {
+  const history = useHistory();
+  const handleGoBack = () => {
+    history.push('/');
+  };
+  const handleLogin = () => {
+    history.push('/admin/basvuru-listesi');
+  };
   return (
     <div className={styles.ButtonContent}>
-      <button type="button" className={styles.Button}>
+      <button type="button" className={styles.Button} onClick={handleGoBack}>
         Geri Dön
       </button>
-      <button type="button" className={styles.Button}>
+      <button type="button" className={styles.Button} onClick={handleLogin}>
         Giriş
       </button>
     </div>
