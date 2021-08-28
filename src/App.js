7c1d6e17cd0d26a073dbc7c325b3e36918cdf6d6
Merve Bacak <nmerveb@gmail.com>
2021-08-28 09:17:57 +0300

    Feat:react-hook-form added to project.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import './App.css';
-import AdminApplicationDetails from './pages/AdminApplicationDetails';
+import RouterConfig from './navigation/RouterConfig';
+import { AddApplication } from './services/fireStore';
 
 function App() {
+  const a = {
+    name: 'testAddApplication',
+    surname: 'testAddApplication',
+    TC: 'testAddApplication',
+    age: 'testAddApplication',
+    address: 'testAddApplication',
+    reason: 'testAddApplication',
+  };
+
+  console.log(AddApplication(a));
   return (
     <div className="App">
-      <AdminApplicationDetails />
+      <RouterConfig />
     </div>
   );
 }
