88c12b9b7f3b00226b6f324b3514f2ec15e9b2cd
Merve Bacak <nmerveb@gmail.com>
2021-08-29 19:22:49 +0300

    Feat: Yup validation schema added.

--- src/pages/Login/InputContent/index.js
+++ src/pages/Login/InputContent/index.js
@@ -1,11 +1,23 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import { useFormContext } from 'react-hook-form';
 import styles from '../Login.module.css';
 
-function InputContent({ label, placeholder }) {
+function InputContent({ label, placeholder, registerName }) {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
   return (
     <div className={styles.LoginContent}>
       <div className={styles.InputLabel}>{label}</div>
-      <input className={styles.Input} type="text" placeholder={placeholder} />
+      <input
+        className={styles.Input}
+        type="text"
+        placeholder={placeholder}
+        {...register(registerName)}
+      />
+      <p className={styles.ValidationError}>{errors[registerName]?.message}</p>
     </div>
   );
 }
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, FormProvider } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import LoginSchema from '../../schema/LoginSchema';
 import LoginIcon from './LoginIcon';
@@ -10,34 +10,33 @@ import ButtonContent from './ButtonContent';
 import styles from './Login.module.css';
 
 function Login() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
+  const methods = useForm({
     resolver: yupResolver(LoginSchema),
   });
-  const onSubmit = (data) => {
-    console.log(data);
-  };
+  const onSubmit = (data) => console.log(data);
   return (
-    <>
+    <FormProvider {...methods}>
       <div className={styles.LoginContainer}>
-        <form onSubmit={handleSubmit(onSubmit)} className={styles.LoginBox}>
+        <form onSubmit={methods.handleSubmit(onSubmit)} className={styles.LoginBox}>
           <LoginIcon />
           <LoginTitle />
           <InputContent
             label="Kullanıcı Adı"
             placeholder="Kullanıcı adınızı giriniz."
-            {...register('username')}
+            registerName="username"
+            type="text"
           />
-          <p className={styles.ValidationError}>{errors.username?.message}</p>
-          <InputContent label="Şifre" placeholder="Şifrenizi giriniz." {...register('password')} />
-          <p className={styles.ValidationError}>{errors.password?.message}</p>
+
+          <InputContent
+            label="Şifre"
+            placeholder="Şifrenizi giriniz."
+            registerName="password"
+            type="password"
+          />
           <ButtonContent buttonType="submit" />
         </form>
       </div>
-    </>
+    </FormProvider>
   );
 }
 
