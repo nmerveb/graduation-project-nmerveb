b78f91fd4581c4194a208f4a23075d8a801bf312
Merve Bacak <nmerveb@gmail.com>
2021-08-29 22:10:07 +0300

    Feat:Auth context added to private route.

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React from 'react';
 import './App.css';
 import RouterConfig from './navigation/RouterConfig';
 import FormInfoProvider from './context/FormInfoContext';
+import AuthProvider from './context/AuthContext';
 
 function App() {
   return (
-    <FormInfoProvider>
-      <div className="App">
-        <RouterConfig />
-      </div>
-    </FormInfoProvider>
+    <AuthProvider>
+      <FormInfoProvider>
+        <div className="App">
+          <RouterConfig />
+        </div>
+      </FormInfoProvider>
+    </AuthProvider>
   );
 }
 
--- src/navigation/PrivateRoute.js
+++ src/navigation/PrivateRoute.js
@@ -1,13 +1,14 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { Redirect, Route, useLocation } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 
 function PrivateRoute({ component: Component }) {
   const location = useLocation();
-  const isLogin = true;
+  const { isAuthenticated } = useAuth();
   return (
     <Route>
-      {isLogin === true ? (
+      {isAuthenticated === true ? (
         <Component />
       ) : (
         <Redirect to={{ pathname: '/admin', state: { from: location } }} />
