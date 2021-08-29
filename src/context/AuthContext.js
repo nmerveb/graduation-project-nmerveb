b158be0f1bc8f07d5d784dfea03c72243abf8213
Merve Bacak <nmerveb@gmail.com>
2021-08-29 21:59:06 +0300

    Feat: AuthContext created for private routes.

---
+++ src/context/AuthContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useState, useContext } from 'react';
+
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = (user) => {
+    if (user.username === 'kodluyoruz' && user.password === 'bootcamp109') {
+      setIsAuthenticated(true);
+      return true;
+    }
+    setIsAuthenticated(false);
+    return false;
+  };
+  const values = { isAuthenticated, setIsAuthenticated, login };
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => useContext(AuthContext);
+export default AuthProvider;
