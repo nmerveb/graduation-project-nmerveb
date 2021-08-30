5bff6337b2ac68054b9d7c83e949b362dd43d49c
Merve Bacak <nmerveb@gmail.com>
2021-08-31 00:31:56 +0300

    Refactor: update func used.

--- src/pages/AdminApplicationDetails/StatusUpdate/index.js
+++ src/pages/AdminApplicationDetails/StatusUpdate/index.js
@@ -1,24 +1,48 @@
-import React from 'react';
+/* eslint-disable react/jsx-props-no-spreading */
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { updateStatus } from '../../../services/fireStore';
 import styles from '../AdminApplicationDetails.module.css';
 
-function StatusUpdate() {
+function StatusUpdate({ id }) {
+  const history = useHistory();
+  const { register, handleSubmit } = useForm();
+  const [success, setSuccess] = useState(false);
+  const onSubmit = (formData) => {
+    updateStatus(id, formData);
+    setSuccess(true);
+    console.log(formData);
+  };
+  const handleClick = () => {
+    history.push('/admin/basvuru-listesi');
+  };
   return (
-    <div className={styles.StatusUpdate}>
+    <form onSubmit={handleSubmit(onSubmit)} className={styles.StatusUpdate}>
       <div className={styles.StatusUpdateTitle}>Güncel Durum</div>
-      <textarea className={styles.DescriptionUpdateInput} type="text" />
+      <textarea
+        className={styles.DescriptionUpdateInput}
+        placeholder="Kullanıcıyı bilgilendirmek için kullanabilirsiniz.Zorunlu değildir."
+        type="text"
+        {...register('description')}
+      />
       <div className={styles.SelectContainer}>
-        <select className={styles.StatusUpdateSelect}>
+        <select className={styles.StatusUpdateSelect} {...register('status')}>
           <option value="pending">İnceleniyor</option>
-          <option value="resolve">Onaylandı</option>
+          <option value="approve">Onaylandı</option>
           <option value="reject">Reddedildi</option>
         </select>
       </div>
       <div className={styles.UpdateBtnContainer}>
-        <button type="button" className={styles.UpdateBtn}>
+        <button type="button" onClick={handleClick} className={styles.UpdateBtn}>
+          Geri Dön
+        </button>
+        <button type="submit" className={styles.UpdateBtn}>
           Gönder
         </button>
       </div>
-    </div>
+      {success && <div className={styles.Success}>İşlem Başarılı</div>}
+    </form>
   );
 }
 
