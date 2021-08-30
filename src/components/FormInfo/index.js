4a5cf4046bf64aac3547c69663e1dd7a6364e197
Merve Bacak <nmerveb@gmail.com>
2021-08-31 00:41:48 +0300

    Refactor: data.status changed with renameStatus

--- src/components/FormInfo/index.js
+++ src/components/FormInfo/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import FormInfoContent from './FormInfoContent';
 import FormDescriptionContent from './FormDescriptionContent';
 import { useFormInfo } from '../../context/FormInfoContext';
+import renameStatus from '../../utils/renameStatus';
 import styles from './FormInfo.module.css';
 
 function FormInfo() {
   const { data } = useFormInfo();
-
+  const status = renameStatus(data.status);
   return (
     <div className={styles.FormInfo}>
       <div className={styles.FormInfoTitle}>Başvuru Bilgileri</div>
@@ -16,7 +17,7 @@ function FormInfo() {
       <FormInfoContent label="Yaş" input={data.age} />
       <FormDescriptionContent label="Başvuru nedeni" input={data.reason} />
       <FormDescriptionContent label="Adres" input={data.address} />
-      <FormInfoContent label="Başvuru Durumu" input={data.status} />
+      <FormInfoContent label="Başvuru Durumu" input={status} />
     </div>
   );
 }
