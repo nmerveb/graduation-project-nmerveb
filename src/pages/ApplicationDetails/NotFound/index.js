56673ffcf09b11e776681c8cd737378cbbadfb25
Merve Bacak <nmerveb@gmail.com>
2021-08-30 13:44:50 +0300

    Feat:Not Found components created.

---
+++ src/pages/ApplicationDetails/NotFound/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from '../ApplicationDetails.module.css';
+
+function NotFound() {
+  return <div className={styles.NotFound}>404 Not Found</div>;
+}
+
+export default NotFound;
