3a95ce3accf774422566eb416e5c780ab834eae6
Merve Bacak <nmerveb@gmail.com>
2021-08-30 21:58:07 +0300

    Fix:Get url params added.

--- src/pages/AdminApplicationDetails/index.js
+++ src/pages/AdminApplicationDetails/index.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useFormInfo } from '../../context/FormInfoContext';
+import { getApplicationFields } from '../../services/fireStore';
 import FormInfo from '../../components/FormInfo';
 import StatusUpdate from './StatusUpdate';
 import styles from './AdminApplicationDetails.module.css';
 
 function AdminApplicationDetails() {
+  const { appId } = useLocation().state;
+  const [loading, setLoading] = useState(true);
+  const { data, setData } = useFormInfo();
+  useEffect(() => {
+    getApplicationFields(appId).then((application) => {
+      if (application) {
+        setData(application);
+        setLoading(false);
+      } else {
+        setLoading(false);
+      }
+    });
+  }, []);
+  console.log(data);
   return (
     <>
       <div className={styles.ApplicationDetailsContainer}>
-        <FormInfo />
-        <StatusUpdate />
+        {loading ? (
+          <div className={styles.Loading}>Loading...</div>
+        ) : (
+          <>
+            <FormInfo />
+            <StatusUpdate />
+          </>
+        )}
       </div>
     </>
   );
