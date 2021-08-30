904268686772cf689c7749a4e6d5cdd8bc784a48
Merve Bacak <nmerveb@gmail.com>
2021-08-31 00:29:39 +0300

    Feat: updateStatus func added.

--- src/services/fireStore.js
+++ src/services/fireStore.js
@@ -5,7 +5,7 @@ const db = firebase.firestore();
 
 export const AddApplication = (application) => {
   const code = codeGenerator();
-  const firstStatus = 'Başvurunuz inceleniyor.';
+  const firstStatus = 'pending';
   const defaultDescription = 'Başvurunuz alınmıştır.';
   db.collection('application-list')
     .add({
@@ -38,13 +38,25 @@ export const getApplicationFields = (queryCode) => {
 export const getPendingList = async () => {
   const applicationRef = db.collection('application-list');
   const pendingList = [];
-  const querySnapshot = await applicationRef.where('status', '==', 'Başvurunuz inceleniyor.').get();
+  const querySnapshot = await applicationRef.where('status', '==', 'pending').get();
   querySnapshot.docs.map((application) => {
     const data = application.data();
     pendingList.push(data);
   });
 
   return pendingList;
+};
+export const updateStatus = async (appId, data) => {
+  const applicationRef = db.collection('application-list');
+  const docId = await applicationRef
+    .where('Id', '==', appId)
+    .get()
+    .then((querySnapshot) => querySnapshot.docs.map((application) => application.id)[0]);
+
+  await applicationRef.doc(docId).update({
+    status: data.status,
+    description: data.description,
+  });
 };
 export const getAuth = async () => {
   const applicationRef = db.collection('application-list');
