13797fa95a3c8820c51615bc78d7c3267b319b14
Merve Bacak <nmerveb@gmail.com>
2021-08-30 13:40:26 +0300

    Feat:ApplicationContainer components created.

---
+++ src/pages/ApplicationDetails/ApplicationContainer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FormInfo from '../../../components/FormInfo';
+import ApplicationStatus from '../ApplicationStatus';
+
+function ApplicationContainer() {
+  return (
+    <>
+      <FormInfo />
+      <ApplicationStatus />
+    </>
+  );
+}
+
+export default ApplicationContainer;
