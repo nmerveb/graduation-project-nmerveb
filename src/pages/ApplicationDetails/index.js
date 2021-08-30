53f9d3f18f347990f0201ec0d537eed6127e7341
Merve Bacak <nmerveb@gmail.com>
2021-08-30 13:50:03 +0300

    Feat: get application method added.

--- src/pages/ApplicationDetails/index.js
+++ src/pages/ApplicationDetails/index.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useFormInfo } from '../../context/FormInfoContext';
+import { getApplicationFields } from '../../services/fireStore';
 import Navbar from '../../components/Navbar';
-import FormInfo from '../../components/FormInfo';
-import ApplicationStatus from './ApplicationStatus';
+import ApplicationContainer from './ApplicationContainer';
+import NotFound from './NotFound';
 import styles from './ApplicationDetails.module.css';
 
 function ApplicationDetails() {
+  const { appId } = useParams();
+  const { setData } = useFormInfo();
+  const [notFound, setNotFound] = useState(false);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    getApplicationFields(appId).then((application) => {
+      if (application) {
+        setData(application);
+        setLoading(false);
+      } else {
+        setNotFound(true);
+        setLoading(false);
+      }
+    });
+  }, []);
   return (
     <>
       <Navbar />
       <div className={styles.ApplicationDetailsContainer}>
-        <FormInfo />
-        <ApplicationStatus />
+        {loading && <div className={styles.Loading}>Loading...</div>}
+        {!loading && (
+          <>
+            {!notFound && (
+              <>
+                <ApplicationContainer />
+              </>
+            )}
+            {notFound && <NotFound />}
+          </>
+        )}
       </div>
     </>
   );
