5bd898598b87f9f5b4bc6ad8b4758d9aa257848a
Merve Bacak <nmerveb@gmail.com>
2021-08-31 14:34:03 +0300

    Fix:.env values moved firebase.config to deploy netlify.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,7 @@
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FİREBASE_API_KEY,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  appId: process.env.REACT_APP_ID,
+  apiKey: 'AIzaSyC2bxmMl1_CPo0-6_ZJmnQsQPGlCm0IpFY',
+  projectId: 'graduation-project-nmerveb',
+  appId: '1:434741468722:web:1f9f76cd5643a4140be62b',
 };
 
 export default firebaseConfig;
